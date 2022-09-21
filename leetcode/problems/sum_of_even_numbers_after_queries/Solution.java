abc873ff2a9f7040331bd009623e021a0b92f561
kiboomhan <kiboomhan@gmail.com>
2022-09-21 11:05:02 +0900

    leetcode 문제 진행

---
+++ leetcode/problems/sum_of_even_numbers_after_queries/Solution.java
@@ -0,0 +1,12 @@
+package problems.sum_of_even_numbers_after_queries;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
+
+    }
+}
